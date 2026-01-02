3528ae35b576395931636814c58c2d8dfbacc9ed
nowesr <nowesr1@hotmail.com>
2026-01-02 05:36:48 +0300

    Initial Commit

---
+++ SnapMemoriesSaver/SnapMemoriesSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapMemoriesSaverApp.swift
+//  SnapMemoriesSaver
+//
+//  Created by Nasser on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnapMemoriesSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
